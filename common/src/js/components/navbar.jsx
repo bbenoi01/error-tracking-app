c6729c9f7d950148202b9105dff634420112434f
Brandon Benoit <b.nutty@gmail.com>
2018-03-03 15:35:44 -0800

    made navbar mobile friendly

--- common/src/js/components/navbar.jsx
+++ common/src/js/components/navbar.jsx
@@ -6,10 +6,16 @@ export default class Navbar extends Component {
             <nav className='navbar fixed-top'>
                 <div className="nav-wrapper">
                     <a href="/" className='brand-logo'style={{paddingLeft: '15px'}}>Error Tracker</a>
-                    <ul className="right hide-onmed-and-down" id='nav-mobile'>
+                    <ul className="right hide-on-med-and-down">
                         <li><a href="#/details">Details</a></li>
                         <li><a href="#/admin">Admin</a></li>
                     </ul>
+                    
+                    <ul className="side-nav" id='nav-mobile'>
+                        <li><a href="#/details">Details</a></li>
+                        <li><a href="#/admin">Admin</a></li>
+                    </ul>
+                    <a href="#" className='button-collapse' data-activates='nav-mobile'><i className="material-icons">menu</i></a>
                 </div>
             </nav>
         );
